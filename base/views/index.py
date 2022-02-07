173d6b283e729d66303f5c4e7b11574ff68ceaf1
skelmis <skelmis.craft@gmail.com>
2022-02-08 01:59:03 +1300

    Try fix site

--- base/views/index.py
+++ base/views/index.py
@@ -1,9 +1,10 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 
 from base.models import Redirect
 
 
 def index_view(request: HttpRequest):
+    return HttpResponse("Hi")
     query_param = request.GET.get("q", "default_redirect")
 
     try:

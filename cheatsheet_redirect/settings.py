0cf36f41ab82c847ef57b7384ddd1d3c0ac007db
skelmis <skelmis.craft@gmail.com>
2022-02-08 00:46:16 +1300

    Turn off debug

--- cheatsheet_redirect/settings.py
+++ cheatsheet_redirect/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,12 +9,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+d3z_1s12f)6(o-))m6m%ex4tarum7#8#l&zt@qq_f&5h-l0sp"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-+d3z_1s12f)6(o-))m6m%ex4tarum7#8#l&zt@qq_f&5h-l0sp",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cheatsheet.koldfusion.xyz", "127.0.0.1"]
 
 
 # Application definition

b64e4126c02743e805b53c00c3faa267188e26a4
skelmis <skelmis.craft@gmail.com>
2022-02-08 02:04:11 +1300

    Remove media files

--- cheatsheet_redirect/settings.py
+++ cheatsheet_redirect/settings.py
@@ -115,9 +115,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = "/uploads/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "base/uploads")
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

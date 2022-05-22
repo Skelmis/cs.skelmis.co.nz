e0699beea6c0fe644cf153c100b086f1ec3f86ff
skelmis <ethan@koldfusion.xyz>
2022-05-22 17:33:03 +1200

    fix: Admin views

--- base/admin.py
+++ base/admin.py
@@ -5,6 +5,12 @@ from base.models import Redirect
 
 @admin.register(Redirect)
 class AdminRedirect(admin.ModelAdmin):
-    list_display = ("query_param", "redirect_url", "last_modified_at", "created_at")
+    list_display = (
+        "query_param",
+        "redirect_url",
+        "times_used",
+        "last_modified_at",
+        "created_at",
+    )
     list_filter = ("last_modified_at", "created_at")
     readonly_fields = ("times_used", "last_modified_at", "created_at")
